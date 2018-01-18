c14ea2ba79529e240c7f9b033d326aa820c4f643
Robert Bond <robert@bryx.com>
2018-01-18 15:27:19 -0500

    Added a list command for raids

--- ootl_bot.py
+++ ootl_bot.py
@@ -1,13 +1,14 @@
 # These are the dependecies. The bot depends on these to function, hence the name. Please do not change these unless your adding to them, because they can break the bot.
 import discord
-import asyncio
+import asyncio # for async things inside discord.py
 from discord.ext.commands import Bot
-from discord.ext import commands
+from discord.ext import commands # imports a bunch of command things
 import platform
 import random
 import copy
 
 client = Bot(description="OOTL Bot by TacticalFruit", command_prefix="%", pm_help = True)
+
 raids = [
     "Vault of Glass",
     "Crota's End",
@@ -17,12 +18,13 @@ raids = [
     "Leviathan: Eater of Worlds"
 ]
 
-"""
-    Client events block
-"""
+############################
+####### Events Block #######
+############################
 
 @client.event
 async def on_ready():
+
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
@@ -30,31 +32,41 @@ async def on_ready():
     print('Use this link to invite {}:'.format(client.user.name))
     print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 
-"""
-    Client commands block
-"""
+############################
+###### Commands Block ######
+############################
 
 @client.command()
 async def ping(*args):
+
     await client.say("Pong!")
 
 @client.command()
 async def name(*args):
+
     await client.say("My name is {}!".format(client.user.name))
 
 # Picks a random raid from the list above.
 # args[0] - Number of raids wanted (Optional, Default=1)
 @client.command()
 async def rr(*args):
-    if len(args) == 0:
-        number_raids = 1
-    else:
-        number_raids = int(args[0])
 
-    raids_copy = copy.deepcopy(raids)
-    random.shuffle(raids_copy)
-    await client.say("Random Raid(s): {}".format(", ".join(raids_copy[0:number_raids])))
+    if len(args) == 0:
+        raids_copy = copy.deepcopy(raids)
+        random.shuffle(raids_copy)
+
+        await client.say("Random Raid(s): {}".format(raids_copy[0]))
+    else:
+        if args[0] == "list":
+            # List out the supported raids
+            await client.say("Supported Raids:\n{}".format("\n".join(raids)))
+        else:
+            number_raids = int(args[0])
+            raids_copy = copy.deepcopy(raids)
+            random.shuffle(raids_copy)
+
+            await client.say("Random Raid(s): {}".format(", ".join(raids_copy[0:number_raids])))
+
 
 # Run the bot with bot token
 client.run('NDAzNTY0MzI0OTAxMjI0NDU4.DUJJug.VfYFg9rgSuaf5i9jdPrG0M41UYI')
-
