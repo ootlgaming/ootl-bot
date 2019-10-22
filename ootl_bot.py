50a114dbae4a9920f143f2b5df21db36b01c40cb
Robert Bond <robert@bryx.com>
2019-10-21 21:59:20 -0700

    Fixed up bot to 1.0

--- ootl_bot.py
+++ ootl_bot.py
@@ -11,12 +11,14 @@ client = Bot(description="OOTL Bot by TacticalFruit", command_prefix="%", pm_hel
 # Remove the normal help command. Our own will be added later
 client.remove_command('help')
 
-tactical_fruit_id = "120317324141133829"
+ootl_guild = None
+
+tactical_fruit_id = 120317324141133829
 tactical_fruit_user = None
 
-ootl_server_id = "327972891029143567"
-ootl_destiny_2_news_channel_id = "405790676416987136"
-ootl_destiny_2_channel_id = "328040568993349632"
+ootl_guild_id = 327972891029143567
+ootl_destiny_2_news_channel_id = 405790676416987136
+ootl_destiny_2_channel_id = 328040568993349632
 
 raids = [
     "Vault of Glass",
@@ -33,33 +35,37 @@ raids = [
 
 @client.event
 async def on_ready():
-
-    print('Loading Discord bot with API version: {}'.format(discord.__version__))
     global tactical_fruit_user
-    members = list(client.get_all_members())
-    tactical_fruit_user = [mem for mem in members if mem.id == tactical_fruit_id][0]
+    global ootl_guild
 
-    print('Logged in as {} (ID: {}) | Connected to {} servers | Connected to {} users'.format(client.user.name, client.user.id, str(len(client.servers)), str(len(set(client.get_all_members())))))
-    print('--------')
-    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
-    print('--------')
-    print('Use this link to invite {}:'.format(client.user.name))
-    print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
+    ootl_guild = client.get_guild(ootl_guild_id)
+
+    if ootl_guild is None:
+        print("couldn't grab guild")
+    else:
+        tactical_fruit_user = [mem for mem in ootl_guild.members if mem.id == tactical_fruit_id][0]
+
+        print('Logged in as {} (ID: {}) | Connected to {} servers | Connected to {} users'.format(client.user.name, client.user.id, str(len(client.guilds)), str(len(set(client.get_all_members())))))
+        print('--------')
+        print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+        print('--------')
+        print('Use this link to invite {}:'.format(client.user.name))
+        print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
 
 @client.event
 async def on_member_join(member):
 
     if tactical_fruit_user is not None:
-        await client.send_message(tactical_fruit_user, "New member joined! {}: {}".format(member.name, member.id))
+        await tactical_fruit_user.send("New member joined! {}: {}".format(member.name, member.id))
 
 @client.event
 async def on_message(message):
-    if (message[0] != "%"): return
+    if not message.content.startswith('%'): return
 
     print("New Message:\n{}: {}".format(message.author.name, message.system_content))
 
     if message.channel.name == "destiny-2-news" and "maintenance" in message.content.lower():
-        await client.send_message(tactical_fruit_user, "New message from: {} saying \n\t".format(message.author, message.content))
+        await tactical_fruit_user.send("New message from: {} saying \n\t".format(message.author, message.content))
 
     await client.process_commands(message)
 
@@ -75,7 +81,9 @@ def is_tactical_fruit(ctx):
 ############################
 
 @client.command()
-async def help(*args):
+async def help(context):
+
+    extra = context.message.content.split(context.prefix + str(context.command))
     """Replies with the help messages requested
 
     Parameters
@@ -84,11 +92,11 @@ async def help(*args):
         * 'ping' -- Replies with the help message for 'ping'
         * 'name' -- Replies with the help message for 'name'
         * 'underbelly' -- Replies with an image of the underbelly
-        * 'rr' -- Replies with the help message for 'rr'
         * 'memIds' -- Replies with the help message for 'memIds'
+        * 'rr' -- Replies with the help message for 'rr'
     """
 
-    if len(args) == 0:
+    if extra[1].strip() == "":
         help_message = \
 """
 ```
@@ -96,8 +104,8 @@ Commands:
   ping       -- Replies with the help message for 'ping'
   name       -- Replies with the help message for 'name'
   underbelly -- Replies with an image of the underbelly
-  rr         -- Replies with the help message for 'rr'
   memIds     -- Replies with the help message for 'memIds'
+  rr         -- Replies with the help message for 'rr'
   
 Use '%help <command>' to find out more about
 a specific command.
@@ -105,7 +113,7 @@ a specific command.
 OOTL-Bot by TacticalFruit
 ```
 """
-    elif args[0] == "ping":
+    elif extra[1].strip() == "ping":
         help_message = \
 """
 ```
@@ -113,11 +121,10 @@ ping -- Pings the bot
 
 Params: None
 Notes:
-    * If the bot is up, it will
-      reply with 'pong'
+    * If the bot is up, it will reply with 'pong'
 ```
 """
-    elif args[0] == "name":
+    elif extra[1].strip() == "name":
         help_message = \
 """
 ```
@@ -126,7 +133,7 @@ name -- Returns the name of the bot
 Params: None
 ```
 """
-    elif args[0] == "memIds":
+    elif extra[1].strip() == "memIds":
         help_message = \
 """
 ```
@@ -135,83 +142,67 @@ memIds -- Returns all server members' name & ID
 Params: None
 ```
 """
-    elif args[0] == "rr":
+    elif extra[1].strip() == "rr":
         help_message = \
 """
 ```
 rr -- Returns a random Destiny raid
 
 Params:
-    * n (int) -- Number of raids to
-      return. Default=1
+    * n (int) -- Number of raids to return. Default=1
     * list -- List the supported raids
 ```
 """
     else:
         help_message = "Command not supported"
 
-    await client.send(help_message)
+    await context.send(help_message)
 
 @client.command()
 @commands.check(is_tactical_fruit)
-async def dump_messages(*args):
-
-    all_messages = client.messages
-    if len(all_messages) == 0:
-        await client.send("No messages found")
-        return
-    all_messages = [msg for msg in all_messages if msg.author.name != "OOTL-Bot"][-10:]
-    messages_dump = "\n".join(["{}: {} | {}/{} --- {}".format(msg.author.name, msg.channel.name, len(msg.attachments), len(msg.embeds), msg.system_content) for msg in all_messages])
-    await client.send("```" + messages_dump + "```")
+async def get_channels(context):
+    await context.send("```" + "\n".join(["{}: {}".format(channel.name, channel.id) for channel in context.guild.channels]) + "```")
 
 @client.command()
 @commands.check(is_tactical_fruit)
-async def get_channels(*args):
-
-    await client.send("```" + "\n".join(["{}: {}".format(channel.name, channel.id) for channel in list(client.get_all_channels())]) + "```")
-
-@client.command()
-@commands.check(is_tactical_fruit)
-async def roles(*args):
-
-    roles = client.get_server(ootl_server_id).role_hierarchy
-    await client.send("```" + "\n".join([role.name for role in roles]) + "```")
+async def roles(context):
+    await context.send("```" + "\n".join([role.name for role in context.guild.roles]) + "```")
 
 @client.command()
 @commands.check(is_tactical_fruit)
-async def members(*args):
+async def members(context):
     """Returns the members from the current server's names and IDs"""
 
-    all_members = list(client.get_all_members())
-    if len(args) > 0:
-        filter_name = args[0]
+    all_members = context.guild.members
+    if len(context.args) > 0:
+        filter_name = context.args[0]
         all_members = [mem for mem in all_members if filter_name.lower() in mem.name.lower()]
 
     longest_name_length = len(max([mem.name for mem in all_members], key=len))
     all_members_ids = ["{:{}} : {:10} : {}".format(mem.name, longest_name_length, mem.top_role.name, mem.id) for mem in all_members]
     all_members_string = "\n".join(all_members_ids)
 
-    await client.send("```" + all_members_string + "```")
+    await context.send("```" + all_members_string + "```")
 
 @client.command()
-async def ping(*args):
+async def ping(context):
     """Pings the bot and replies with Pong"""
 
-    await client.send("Pong!")
+    await context.send("Pong!")
 
 @client.command()
-async def name(*args):
+async def name(context):
     """Says back the bot's name"""
 
-    await client.send("My name is {}!".format(client.user.name))
+    await context.send("My name is {}!".format(context.user.name))
 
-async def underbelly(*args):
+async def underbelly(context):
     """Replies with an image of the underbelly"""
 
-    await client.send("Leviathen - underbelly", { files: ["https://media.discordapp.net/attachments/403568301164462080/635681911808720917/image0.jpg"] })
+    await context.send("Leviathen - underbelly", file="https://media.discordapp.net/attachments/403568301164462080/635681911808720917/image0.jpg")
 
 @client.command()
-async def rr(*args):
+async def rr(context):
     """Picks a random raid from the 'raids' list
 
     Parameters
@@ -221,21 +212,21 @@ async def rr(*args):
         * 'list' -- lists out the supported raids
     """
 
-    if len(args) == 0:
+    if len(context.args) == 0:
         raids_copy = copy.deepcopy(raids)
         random.shuffle(raids_copy)
 
-        await client.send("Random Raid(s): {}".format(raids_copy[0]))
+        await context.send("Random Raid(s): {}".format(raids_copy[0]))
     else:
-        if args[0] == "list":
+        if context.args[0] == "list":
             # List out the supported raids
-            await client.send("Supported Raids:\n{}".format("\n".join(raids)))
+            await context.send("Supported Raids:\n{}".format("\n".join(raids)))
         else:
-            number_raids = int(args[0])
+            number_raids = int(context.args[0])
             raids_copy = copy.deepcopy(raids)
             random.shuffle(raids_copy)
 
-            await client.send("Random Raid(s): {}".format(", ".join(raids_copy[0:number_raids])))
+            await context.send("Random Raid(s): {}".format(", ".join(raids_copy[0:number_raids])))
 
 # Run the bot with bot token
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
